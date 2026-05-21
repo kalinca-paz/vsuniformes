4c6b6b7b2e306a3a482d878503b72d497ce9d002
kalinca-paz <zzapkali@gmail.com>
2026-05-21 18:50:17 -0300

    tela sobre e orcamento

---
+++ view/orcamento.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Orçamento</title>
+</head>
+
+<body>
+    <section>
+
+        <h2>
+            Escolha seu orçamento
+        </h2>
+
+        <div class="container-orcamentos">
+            
+            <div class="card-orca">
+                <h3>Bordados</h3>
+                <table class="tabela-orca">
+                    <thead>
+                        <tr>
+                            <th>Tipo de Bordado</th>
+                            <th>Quantidade</th>
+                            <th>Tamanho</th>
+                            <th>Preço</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Frente</td>
+                            <td>10(min.)</td>
+                            <td>10cm x 10cm</td>
+                            <td>R$ 10,00</td>
+                        </tr>
+                        <tr>
+                            <td>Bolso</td>
+                            <td>10(min.)</td>
+                            <td>8cm x 8cm</td>
+                            <td>R$ 20,00</td>
+                        </tr>
+                        <tr>
+                            <td>Calça</td>
+                            <td>10(min.)</td>
+                            <td>12cm x 12cm</td>
+                            <td>R$ 30,00</td>
+                        </tr>
+                        <tr>
+                            <td>Costa</td>
+                            <td>10(min.)</td>
+                            <td>25cm x 25cm</td>
+                            <td>R$ 40,00</td>
+                        </tr>
+                        <tr>
+                            <td>Logo Especial</td>
+                            <td>10(min.)</td>
+                            <td>30cm x 30cm</td>
+                            <td>R$ 50,00</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="card-orca">
+                <h3>Camisaria</h3>
+                <table class="tabela-orca">
+                    <thead>
+                        <tr>
+                            <th>Tipo de Camisa</th>
+                            <th>Quantidade</th>
+                            <th>Tamanho</th>
+                            <th>Preço</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Camiseta Básica</td>
+                            <td>Mínimo 10</td>
+                            <td>P ao GG</td>
+                            <td>R$ 20,00</td>
+                        </tr>
+                        <tr>
+                            <td>Camiseta Polo</td>
+                            <td>Mínimo 10</td>
+                            <td>P ao GG</td>
+                            <td>R$ 30,00</td>
+                        </tr>
+                        <tr>
+                            <td>Camisa Social</td>
+                            <td>Mínimo 10</td>
+                            <td>P ao GG</td>
+                            <td>R$ 40,00</td>
+                        </tr>
+                        <tr>
+                            <td>Camisa Dry Fit</td>
+                            <td>Mínimo 10</td>
+                            <td>P ao GG</td>
+                            <td>R$ 50,00</td>
+                        </tr>
+                        <tr>
+                            <td>Camisa Premium</td>
+                            <td>Mínimo 10</td>
+                            <td>P ao EXG</td>
+                            <td>R$ 60,00</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="card-orca">
+                <h3>Peças Especiais</h3>
+                <table class="tabela-orca">
+                    <thead>
+                        <tr>
+                            <th>Peça</th>
+                            <th>Quantidade</th>
+                            <th>Tamanho</th>
+                            <th>Preço</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Jaleco</td>
+                            <td>Mínimo 10</td>
+                            <td>P ao EXG</td>
+                            <td>R$ 40,00</td>
+                        </tr>
+                        <tr>
+                            <td>Avental</td>
+                            <td>Mínimo 10</td>
+                            <td>Único</td>
+                            <td>R$ 50,00</td>
+                        </tr>
+                        <tr>
+                            <td>Blazer</td>
+                            <td>Mínimo 10</td>
+                            <td>P ao EXG</td>
+                            <td>R$ 60,00</td>
+                        </tr>
+                        <tr>
+                            <td>Porta Corpo</td>
+                            <td>Mínimo 10</td>
+                            <td>Padrão</td>
+                            <td>R$ 70,00</td>
+                        </tr>
+                        <tr>
+                            <td>Touca</td>
+                            <td>Mínimo 10</td>
+                            <td>Único</td>
+                            <td>R$ 80,00</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+
+    </section>
+</body>
+
+</html>
