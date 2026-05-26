4d912301edb72951d896001b72908fce3af4712b
Tgnsk8 <tgnsk8@gmail.com>
2026-05-26 09:30:46 -0300

    adição do painel admin

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
-
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="shortcut icon" href="/vsuniformes/img/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="/vsuniformes/css/style.css">
 <head>
     <!-- CABEÇALHO PADRÃO HTML -->
-    <?php include 'view/includes/head.php';?>
+    
     <title>Início - VS Uniformes</title>
 </head>
 <body>
@@ -14,7 +21,31 @@
         <a class="btn-orcamento" href="view/orcamento.php"> Faça seu orçamento </a>
     </section>
     <!-- MENU -->
- <?php include 'view/includes/menu.php';?>
+<!-- menu.php -->
+<header>
+    <h1>VS uniformes</h1>
+    <nav>
+        <ul>
+            <li><a href="/vsuniformes/index.php">Início</a></li>
+            <li><a href="/vsuniformes/view/sobre.php">Sobre</a></li>
+            <li><a href="/vsuniformes/view/portifolio.php">Portifólio</a></li> 
+            <li><a href="/vsuniformes/view/login.php">Login</a></li>
+        </ul>
+    </nav>
+</header>
+<?php
+if (isset($SESSION['nome'])) {
+    echo "Bem-vindo," . $_SESSION['nome'];
+    if ($_SESSION['tipo']=='admin') {
+        echo "<a href='view/painelAdmin.php'>Painel Admin</a> ";
+    echo "<a href='controller/logout.php'>Sair</a> ";
+    } else {
+        echo "<a href='view/login.php/'>Login</a>";
+    }
+}
+?>
+  
+
     <!-- SOBRE -->
     <section>
         <div class="container">
